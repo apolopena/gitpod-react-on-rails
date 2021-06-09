e40413ca7eee47ec824b51fb2c92a6055dceae42
Apolo Pena <3060702+apolopena@users.noreply.github.com>
2021-06-09 09:27:37 -0700

    stub

---
+++ .gp/bash/init-scaffolding.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# SPDX-License-Identifier: MIT
+# Copyright © 2021 Apolo Pena
+#
+# init-scaffolding.sh
+# Description:
+# Conditionally Installs scaffolding
+
+# Load logger
+. .gp/bash/workspace-init-logger.sh
+
+# Load spinner
+. .gp/bash/spinner.sh
+
