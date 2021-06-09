9e9659d3b95c7d27ad8b89bf44ce2e7427b03742
Apolo Pena <3060702+apolopena@users.noreply.github.com>
2021-06-09 09:47:06 -0700

    add inited hook back in

--- .gp/bash/start-server.sh
+++ .gp/bash/start-server.sh
@@ -10,12 +10,17 @@
 # Load spinner
 . .gp/bash/spinner.sh
 
+if [[ $(bash .gp/bash/helpers.sh is_inited) == 1 ]]; then
+  gp sync-done gitpod-inited
+fi;
+
 UP=$(pgrep postgres | wc -l)
 if [[ $UP -ne 1 ]]; then
   start_spinner "Initializing PostgreSQL..." &&
   gp await-port 5432 &&
   stop_spinner $?
 fi 
+
 gp await-port 5432 &&
 start_spinner "Starting server when system is ready..." &&
 gp sync-await gitpod-inited &&
