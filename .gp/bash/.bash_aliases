65cb7019b47f975887164284d05a033a2031eada
apolopena <3060702+apolopena@users.noreply.github.com>
2021-06-09 23:18:33 -0700

    ➕ ADD: dserver: start or stop the development server

--- .gp/bash/.bash_aliases
+++ .gp/bash/.bash_aliases
@@ -3,3 +3,6 @@
 
 # opens or refreshes the preview browser. Try op --help
 alias op='f(){ bash "$GITPOD_REPO_ROOT"/.gp/bash/open-preview.sh "$@";  unset -f f; }; f'
+
+# starts or stops the development server. try dserver --help
+alias dserver='f(){ bash "$GITPOD_REPO_ROOT"/.gp/bash/dserver.sh "$@";  unset -f f; }; f'
---
+++ .gp/bash/dserver.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#
+# SPDX-License-Identifier: MIT
+# Copyright © 2021 Apolo Pena
+#
+# serverctl.sh
+# Description:
+# Starts puma and or webpack-dev-server via foreman
+#
+# Notes:
+# The foreman gem will be installed if the current tty does not find an installation of foreman
+# The server will not be started if an instace of it is already running
+
+# Load spinner
+. .gp/bash/spinner.sh
+
+c_red='\e[38;5;124m'
+c_end='\e[0m'
+
+red() {
+  echo "$c_red$1$c_end"
+}
+
+_version() {
+  echo -e "dserver (MIT license) v1.0.0\nCopyright (C) 2021 Apolo Pena "
+}
+
+_help() {
+  echo -e "Start and stop the development server"
+  echo -e "Usage:/n  dserver <[-h | --help | start | stop]>"
+  echo -e "Example 1: Start puma and webpack-dev-server via foreman"
+  echo -e "dserver start"
+  echo -e "Example 2: Stop puma and webpack-dev-server via foreman"
+  echo -e "dserver stop"
+}
+
+[[ $1 == '-v' || $1 == '--version' ]] && _version && exit 0
+[[ -z $1 || $1 == '-h' || $1 == '--help' ]] && _help && exit 0
+[[ $1 != 'start' && $1 != 'stop' ]] && red "Invalid command or flag: $1" && _help && exit 1
+[[ -n $(pregep foreman) ]] && red "Forman is already running" && exit 1
+[[ ! -f Procfile-dev-hmr ]] && red "Procfile-dev-hmr is required but not found, aborted." && exit 1
+
+if [[ $(gem list foreman -i) == 'false' ]]; then
+  msg="Installing gem: foreman"
+  start_server "$msg"
+  if gem install foreman; then stop_spinner 0; else stop_spinner 1 && exit 1; fi
+fi
+
+foreman start -f Procfile.dev-hmr
